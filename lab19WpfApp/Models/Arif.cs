d271119a20bafc0e548a1d672e89460798e73444
Екатерина Горяинова <kateas27@mail.ru>
2022-01-09 19:30:31 +0200

    Добавьте файлы проекта.

---
+++ lab19WpfApp/Models/Arif.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab19WpfApp.Models
+{
+    static class Arif
+    {
+        public static double Add(double a) => 2 * Math.PI * a;
+    }
+}
---
+++ lab19WpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using lab19WpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace lab19WpfApp.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            //Length = Radius*Math.PI*2;
+            Length = Arif.Add(Radius);
+        }
+
+        private bool CanAddCommandExecute(object p)
+        {
+            if (radius != 0)
+            {
+                return true; 
+            }
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
